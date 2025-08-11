07fe27a56b98d83eb3286c71490c83d561a26a2d
mikei72 <2916625750@qq.com>
2025-08-11 21:26:43 +0800

    update

--- pipeline/super_resolution_pipeline.py
+++ pipeline/super_resolution_pipeline.py
@@ -136,11 +136,11 @@ class SuperResolutionPipeline:
         if output_path:
             self._save_final_result(hr_final, output_path)
 
-        from utils.metrics_utils import calculate_metrics
+        """from utils.metrics_utils import calculate_metrics
         metrics1 = calculate_metrics('examples/hr_base_demo.png', 'examples/gt.png', 10, True)
         metrics2 = calculate_metrics('examples/sr_demo.png', 'examples/gt.png', 10, True)
         print(f'PSNR: {metrics1["psnr"]:.4f} dB, SSIM: {metrics1["ssim"]:.4f}')
-        print(f'PSNR: {metrics2["psnr"]:.4f} dB, SSIM: {metrics2["ssim"]:.4f}')
+        print(f'PSNR: {metrics2["psnr"]:.4f} dB, SSIM: {metrics2["ssim"]:.4f}')"""
         
         # 返回结果和处理信息
         process_info = {
